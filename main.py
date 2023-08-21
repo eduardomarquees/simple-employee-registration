e96a2e5375ea4e7e6b5df7b6152849d45dcbaefe
eduardomarquees <eduardomarques.dev01@gmail.com>
2023-08-21 15:58:59 -0300

    fazendo testes

--- main.py
+++ main.py
@@ -5,23 +5,30 @@ class Funcionario:
         self.nome = nome
         self.idade = idade
         self.cargo = cargo
-        self.matricula = matricula
-        self.cpf = cpf
-        
+
+
 def cadastrar_funcionario():
     nome = input('Digite o nome do funcionário: ')
     idade = int(input('Digite a idade do funcionário'))
     cargo = input('Digite o cargo do funcionário: ')
-    matricula = int(input('Digite a matricula do funcionário: '))
-    cpf = int(input('Digite o CPF do funcionário: '))
 
-    novo_funcionario = Funcionario(nome, idade, cargo, matricula, cpf)
+    novo_funcionario = Funcionario(nome, idade, cargo)
     return novo_funcionario
+
 
 def listar_funcionario(funcionarios):
     print("\nLista de funcionários: ")
     for i, funcionario in enumerate(funcionarios, start=1):
-        print(f"{i}, Nome: {funcionario.nome}, Idade: {funcionario.idade}, Cargo: {funcionario.cargo}, Matricula: {funcionario.matricula},"
-            f"CPF: {funcionario.cpf}")
+        print(
+            f"{i}, Nome: {funcionario.nome}, Idade: {funcionario.idade}, Cargo: {funcionario.cargo}")
+
 
 def excluir_funcionario(funcionarios):
+    listar_funcionario(funcionarios)
+    indice = int(input('Digite o número do funcionário que deseja excluir: '))
+
+    if 0 <= indice <len(funcionarios):
+        funcionarios.pop(indice)
+        print('Funcionário excluído com sucesso.')
+    else:
+        print('Indice invalida.')
